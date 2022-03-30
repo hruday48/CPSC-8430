9477924dfde2ac0fbd25e9853b207be05e8b5977
hruday48 <78971804+hruday48@users.noreply.github.com>
2022-03-29 23:28:27 -0400

    Add files via upload

---
+++ hw2/hw2_1/hw2_seq_to_seq.sh
@@ -0,0 +1,2 @@
+python python sequence.py /home/hsantim/MLDS_hw2_1_data/training_data/feat/ /home/hsantim/MLDS_hw2_1_data/training_label.json
+python train.py /home/hsantim/MLDS_hw2_1_data/training_data/feat/ /home/hsantim/MLDS_hw2_1_data/training_label.json ./hsantim_output.txt
